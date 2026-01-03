506bca14bbf5f96144e0398bcffc6540c34a7a18
AhmedShahan <shahan.ahmed001@gmail.com>
2026-01-03 15:55:08 +0600

    requirements are installed and basic chatbot using langchain

---
+++ chatbot/langchain.py
@@ -0,0 +1,14 @@
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load the API key
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.5
+)
+
+# Ask a question
+response = llm.invoke("Write a short story about a cat in a futuristic city.")
+print(response.content)
